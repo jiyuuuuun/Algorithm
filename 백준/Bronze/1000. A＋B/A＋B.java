94e3ebee46760ca68dbe3cf94efca29b9d92937a
황지윤 <105794920+jiyuuuuun@users.noreply.github.com>
2025-06-03 21:06:55 +0900

    [Bronze V] Title: A+B, Time: 176 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();  // 정수 하나 입력
+        int m = sc.nextInt();  // 정수 하나 입력
+        System.out.println(n + m);
+    }
+}
