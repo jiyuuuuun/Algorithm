8d89a3df79c5e1b41ea37212e96be983fcaaa142
황지윤 <105794920+jiyuuuuun@users.noreply.github.com>
2025-06-04 16:57:04 +0900

    [level 1] Title: 추억 점수, Time: 33.50 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -5,11 +5,9 @@ class Solution {
             for(int j=0;j<photo[i].length;j++){
                 for(int z=0;z<name.length;z++){
                     if(photo[i][j].equals(name[z])){
-                        answer[i]+=yearning[z];
-                        
-}
-}
-              
+                        answer[i]+=yearning[z];     
+                    }
+                }              
             }
         }
         return answer;
